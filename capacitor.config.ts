ca83031fe374f37658b85d5df2af6d02b4547321
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2025-06-30 16:08:49 +0000

    refactor:renew code

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cubix.app',
+  appName: 'Cubix',
+  webDir: 'www'
+};
+
+export default config;
